cd0497906b320f7c60feaef6faad2b3757756bd2
Daan <daan.vanhoof01@gmail.com>
2026-07-24 00:39:00 +0200

    Fix bug where if no abilities are available event screen would crash

--- scripts/events/event_screen.gd
+++ scripts/events/event_screen.gd
@@ -5,7 +5,7 @@ extends Control
 
 var available_events: Array[GameEvent] = []
 
-func _get_removeable_ability() -> Ability:
+func _get_removeable_ability_name() -> String:
     var has_removeable_ability: bool = false
     for ability in GameState.player_stats.abilities.values():
         if ability.ability_type != Ability.AbilityType.NORMAL:
@@ -20,29 +20,31 @@ func _get_removeable_ability() -> Ability:
     while true:
         var found_ability: Ability = GameState.player_stats.abilities.values()[index_to_remove]
         if found_ability.ability_type != Ability.AbilityType.NORMAL:
-            return found_ability
+            return found_ability.name
         index_to_remove += 1
         if index_to_remove == len(GameState.player_stats.abilities):
             index_to_remove = 0
     return null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     accept_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
     reject_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-    var ability_1 = _get_removeable_ability().name
+    var ability_to_remove: String = _get_removeable_ability_name()
     #event1
-    var existential_event: GameEvent = GameEvent.new()
-    existential_event.text = "
-        You eat a strange mushroom.
-        Moments later you're wondering whether your 
-        enemies are merely projections of your own
-        subconscious.
+    if ability_to_remove:
+        var existential_event: GameEvent = GameEvent.new()
+        existential_event.text = "
+            You eat a strange mushroom.
+            Moments later you're wondering whether your 
+            enemies are merely projections of your own
+            subconscious.
 
-        You gain + 25 health but can't ever cast " + ability_1 + "."
-    existential_event.accept_text = "Accept enlightment"
-    existential_event.reject_text = "Purge"
-    existential_event.take_action_func = Callable(existential_event_callback.bind(ability_1))
-    available_events.append(existential_event)
+            You gain + 25 health but can't ever cast " + ability_to_remove + "."
+        existential_event.accept_text = "Accept enlightment"
+        existential_event.reject_text = "Purge"
+        existential_event.take_action_func = Callable(existential_event_callback.bind(ability_to_remove))
+        available_events.append(existential_event)
     
     #event2
     var unionized_event: GameEvent = GameEvent.new()
@@ -54,7 +56,7 @@ func _ready() -> void:
         You gain + 15 on your armor when you help unionizing the bandits"
     unionized_event.accept_text = "Unionize bandits!"
     unionized_event.reject_text = "Not my problem"
-    unionized_event.take_action_func = Callable(unionized_event_callback.bind(ability_1))
+    unionized_event.take_action_func = Callable(unionized_event_callback.bind(ability_to_remove))
     available_events.append(unionized_event)
     
     #event3
@@ -65,7 +67,7 @@ func _ready() -> void:
         Help move the old lady and receive +25 on your magic shield."
     helpmove_event.accept_text = "Move the couch"
     helpmove_event.reject_text = "I don't have time"
-    helpmove_event.take_action_func = Callable(helpmove_event_callback.bind(ability_1))
+    helpmove_event.take_action_func = Callable(helpmove_event_callback.bind(ability_to_remove))
     available_events.append(helpmove_event)
     
     
@@ -82,21 +84,24 @@ func choose_random_event():
 
 #event1callback
 func existential_event_callback(ability_to_toss: String):
-    GameState.player_stats.abilities[ability_to_toss].is_disabled = true
+    if ability_to_toss != null:
+        GameState.player_stats.abilities[ability_to_toss].is_disabled = true
     GameState.player_stats.max_health += 25
     GameState.player_stats.health += 25
     SceneLoader.load_scene("res://scenes/map/map.tscn")
 
 #event2callback
 func unionized_event_callback(ability_to_toss: String):
-    GameState.player_stats.abilities[ability_to_toss].is_disabled = true
+    if ability_to_toss != null:
+        GameState.player_stats.abilities[ability_to_toss].is_disabled = true
     GameState.player_stats.max_armor += 15
     GameState.player_stats.armor += 15
     SceneLoader.load_scene("res://scenes/map/map.tscn")
 
 #event3callback
 func helpmove_event_callback(ability_to_toss: String):
-    GameState.player_stats.abilities[ability_to_toss].is_disabled = true
+    if ability_to_toss != null:
+        GameState.player_stats.abilities[ability_to_toss].is_disabled = true
     GameState.player_stats.max_shield += 25
     GameState.player_stats.shield += 25
     SceneLoader.load_scene("res://scenes/map/map.tscn")
