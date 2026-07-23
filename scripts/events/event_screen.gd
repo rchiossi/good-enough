b8121f20120a6280a090cb102132ebad34922c89
Victoria Popova <victoria.i.popova@gmail.com>
2026-07-23 22:25:31 +0200

    new events added

--- scripts/events/event_screen.gd
+++ scripts/events/event_screen.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
     reject_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
     var index_to_remove = randi_range(0, len(GameState.player_stats.abilities.values())-1)
     var ability_1 = GameState.player_stats.abilities.values()[index_to_remove].name
+    
+    #event1
     var existential_event: GameEvent = GameEvent.new()
     existential_event.text = "
         You eat a strange mushroom.
@@ -18,13 +20,37 @@ func _ready() -> void:
         enemies are merely projections of your own
         subconscious.
 
-        You gain +5 health but can't ever cast " + ability_1 + "."
+        You gain + 25 health but can't ever cast " + ability_1 + "."
     existential_event.accept_text = "Accept enlightment"
     existential_event.reject_text = "Purge"
     existential_event.take_action_func = Callable(existential_event_callback.bind(ability_1))
     available_events.append(existential_event)
+    
+    #event2
+    var unionized_event: GameEvent = GameEvent.new()
+    unionized_event.text = "
+        You stumble upon a gang of bandits picketing instead of pillaging.
+        <<Fair pay for fair plunder>>
+        Their leader hands you a pamphlet.
+
+        You gain + 15 on your armor when you help unionizing the bandits"
+    unionized_event.accept_text = "Unionize bandits!"
+    unionized_event.reject_text = "Not my problem"
+    unionized_event.take_action_func = Callable(unionized_event_callback.bind(ability_1))
+    available_events.append(unionized_event)
+    
+    #event3
+    var helpmove_event: GameEvent = GameEvent.new()
+    helpmove_event.text = "
+        You meet an old lady who's trying to move her couch into a treehouse. 
+        
+        Help move the old lady and receive +25 on your magic shield."
+    helpmove_event.accept_text = "Move the couch"
+    helpmove_event.reject_text = "I don't have time"
+    helpmove_event.take_action_func = Callable(helpmove_event_callback.bind(ability_1))
+    available_events.append(helpmove_event)
     choose_random_event()
-    # setup event
+
 
 func choose_random_event():
     var event_index = randi_range(0, len(available_events)-1)
@@ -34,9 +60,22 @@ func choose_random_event():
     accept_button.pressed.connect(picked_event.take_action_func)
     reject_button.text = picked_event.reject_text
 
+#event1callback
 func existential_event_callback(ability_to_toss: String):
     GameState.player_stats.abilities[ability_to_toss].is_disabled = true
-    GameState.player_stats.health = min(GameState.player_stats.health + 5, GameState.player_stats.max_health)
+    GameState.player_stats.health = min(GameState.player_stats.health + 25, GameState.player_stats.max_health)
+    SceneLoader.load_scene("res://scenes/map/map.tscn")
+
+#event2callback
+func unionized_event_callback(ability_to_toss: String):
+    GameState.player_stats.abilities[ability_to_toss].is_disabled = true
+    GameState.player_stats.armor = min(GameState.player_stats.armor + 15, GameState.player_stats.max_armor)
+    SceneLoader.load_scene("res://scenes/map/map.tscn")
+
+#event3callback
+func helpmove_event_callback(ability_to_toss: String):
+    GameState.player_stats.abilities[ability_to_toss].is_disabled = true
+    GameState.player_stats.shield = min(GameState.player_stats.shield + 25, GameState.player_stats.max_shield)
     SceneLoader.load_scene("res://scenes/map/map.tscn")
 
 func _on_reject_button_pressed() -> void:
