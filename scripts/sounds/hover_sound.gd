01c78fd410fa3c319fcf39caa2601841ff20b48b
Alexandru Surdu <alin.alex.92@gmail.com>
2026-07-23 20:49:36 +0200

    update bus for hover and press sounds

--- scripts/sounds/hover_sound.gd
+++ scripts/sounds/hover_sound.gd
@@ -1,8 +1,8 @@
 extends Control
 class_name HoverSound
 
-#@export var pressed_sound: AudioStream = load("res://resources/effects/button_press.ogg")
 @export var hover_sound: AudioStream = preload("uid://dgnmyji1yijho")
+@export var bus: StringName = "Sfx"
 
 var player: AudioStreamPlayer2D
 
@@ -11,15 +11,12 @@ var _target : Control
 func _ready() -> void:
     _target = get_parent()
     player = AudioStreamPlayer2D.new()
+    player.bus = bus
     add_child(player)
     player.stream = hover_sound
 
     mouse_filter = Control.MOUSE_FILTER_PASS
-    #_target.pressed.connect(on_button_pressed)
     _target.mouse_entered.connect(on_hover)
-
-#func on_button_pressed()->void:
-    #SoundManager.play_ui_sound(pressed_sound)
 
 func on_hover():
     player.play()
