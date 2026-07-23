588210859cce85b7366f7ca326164737c7b0fe24
Victoria Popova <victoria.i.popova@gmail.com>
2026-07-23 17:44:25 +0200

    make event screen generic and pick random events

---
+++ scripts/events/game_event.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name GameEvent
+
+var title: String = ""
+var text: String = ""
+var reject_text: String = ""
+var accept_text: String = ""
+var take_action_func: Callable = Callable()
