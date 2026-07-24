ec69387568129b4400c2472aec549c89e2715b0d
Alexandru Surdu <alin.alex.92@gmail.com>
2026-07-24 14:45:51 +0200

    map v3.234 improve fire

--- scripts/map/map.gd
+++ scripts/map/map.gd
@@ -32,7 +32,12 @@ func _process(_delta: float) -> void:
         current_node.global_position.y + current_node.size.y / 2
     )
     if GameState.current_position in GameState.nodes:
-        %FirePanel.custom_maximum_size.x = max(GameState.nodes[GameState.current_position].global_position.x, 0)
+        %FirePanel.global_position.x = 0
+        %FirePanel.global_position.y = 0
+        %FirePanel.size.x = max(
+            GameState.nodes[GameState.current_position].global_position.x + GameState.nodes[GameState.current_position].size.x / 2,
+            0
+        )
 
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("ui_cancel"):
