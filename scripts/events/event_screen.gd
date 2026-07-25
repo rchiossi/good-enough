6eb2432b3a7d7373820f39ee8da74450e0b224f0
Victoria Popova <victoria.i.popova@gmail.com>
2026-07-25 21:43:56 +0200

    all events are in our favor

--- scripts/events/event_screen.gd
+++ scripts/events/event_screen.gd
@@ -73,49 +73,67 @@ func _ready() -> void:
         unionized_event.accept_text += "\n[color=red](Gain +15 Armor, possibly lose [i]%s[/i])[/color]" % ability_to_remove
     unionized_event.reject_text = "Hand over one ability"
     unionized_event.take_action_func = Callable(unionized_event_callback.bind(ability_to_remove))
-    available_events.clear()
     available_events.append(unionized_event)
     
     #event3
     var helpmove_event: GameEvent = GameEvent.new()
     helpmove_event.text = "
-        You meet an old lady who's trying to move her couch into a treehouse. 
+    You find a granny struggling to haul an oversized couch into a treehouse.
+
+    She notices you.
+    <<Oh, perfect! A strong young lad.
+    This won't take but a minute.>>
+
+    You have a feeling she's said that before..
         
-        Help move the old lady and receive +25 on your magic shield."
-    helpmove_event.accept_text = "Move the couch"
-    if ability_to_remove:
-        helpmove_event.accept_text += "\n[color=red](Lose [i]%s[/i])[/color]" % ability_to_remove
-    helpmove_event.reject_text = "I don't have time"
-    helpmove_event.take_action_func = Callable(helpmove_event_callback.bind(ability_to_remove))
+    Help granny and gain +25 magic shield. 
+    However, there is a 50% chance you'll have to move grannys entire house.
+    Causing you to lose -25 magic armor.
+    
+    Reject granny and she'll take an ability of your choice to teach you to respect your elders.
+    "
+    helpmove_event.accept_text = "Lift with your legs"
+    helpmove_event.accept_text += "\n[color=red](May gain or lose [i]25 Magic Shield[/i])[/color]"
+    helpmove_event.reject_text = "Suddenly remember an appointment"
+    helpmove_event.take_action_func = Callable(helpmove_event_callback)
     available_events.append(helpmove_event)
     
     #event4
     var tax_event: GameEvent = GameEvent.new()
     tax_event.text = "
-        The royal tax collector approaches.
-        The count requires a tax on.. existing
+    A royal tax collector blocks your path.
+    He unfurls a scroll several meters long.
+    <<By order of the Count, all citizens are required to pay the Royal Existence Tax.>>
+
+    Yes, you are being taxed... for existing.
+
+    Unfortunately, your existence has been more expensive than expected.
         
-        Pay the tax, but you can't afford a meal for a week (-20 Health) 
-        or Refuse to pay and fight the tax collector. You lose 1 ability.
+    Pay the tax, but you can't afford a meal for a week (-20 Health)  
+    or hand over 1 ability.
     "
-    tax_event.accept_text = "Pay the tax"
+    tax_event.accept_text = "Pay the Existence Tax"
     tax_event.accept_text += "\n[color=red](Lose [i]-20 HP[/i])[/color]"
-    tax_event.reject_text = "Refuse to pay"
+    tax_event.reject_text = "I have.. other assets"
     tax_event.take_action_func = Callable(tax_event_callback)
     available_events.append(tax_event)
     
     #event5
     var wildgoose_event: GameEvent = GameEvent.new()
     wildgoose_event.text = "
-        A wild mega goose blocks the road. 
+        A wild Mega Goose blocks the road. 
         He's an agressive boy and looks very confident. 
         
         Fight the wild goose and damage your armor (-15 Armor)
-        Give it bread, you coward. You lose 1 ability.
+        
+        Give the Mega Goose some bread as tribute. 
+        The goose now considers itself your superior and demands
+        a permanent sign of respect. Lose 1 ability
+        
     "
-    wildgoose_event.accept_text = "You won. Barely.. "
+    wildgoose_event.accept_text = "Challenge the Goose"
     wildgoose_event.accept_text += "\n[color=red](Lose [i]-15 Armor[/i])[/color]"
-    wildgoose_event.reject_text = "The goose considers you beneath violence."
+    wildgoose_event.reject_text = "Pay the Goose Tax"
     wildgoose_event.take_action_func = Callable(wildgoose_event_callback)
     available_events.append(wildgoose_event)
    
@@ -128,21 +146,22 @@ func _ready() -> void:
         <<definitley not cursed>>
         
         Throw a coin and see what happens.. 
-        If you do nothing you lose 1 ability.        
+        
+        Ignore the well and lose 1 ability.        
     "
-    well_event.accept_text = "The well appreciates your donation. Your body.. maybe not"
-    well_event.accept_text += "\n[color=red](70% you gain [i]20 Health[/i] 30% you lose [i]20 Health[/i])[/color]"
-    well_event.reject_text = "Keep walking"
+    well_event.accept_text = "Trust the Definitely-Not-Cursed Well"
+    well_event.accept_text += "\n[color=red](30% you gain [i]20 Health[/i] 70% you lose [i]20 Health[/i])[/color]"
+    well_event.reject_text = "Pretend You Didn't See It"
     well_event.take_action_func = Callable(well_event_callback)
+    available_events.clear()
     available_events.append(well_event)
-    
+
     choose_random_event() 
 
 func choose_random_event():
     var event_index = randi_range(0, len(available_events)-1)
     var picked_event = available_events[event_index]
     %FirstPageText.text = picked_event.text
-    #accept_button.text = picked_event.accept_text
     %AcceptButtonText.text = picked_event.accept_text
     
     accept_button.pressed.connect(picked_event.take_action_func)
@@ -169,11 +188,14 @@ func unionized_event_callback(ability_to_toss: String):
     SceneLoader.load_scene("res://scenes/map/map.tscn")
 
 #event3callback
-func helpmove_event_callback(ability_to_toss: String):
-    if ability_to_toss != null:
-        GameState.player_stats.abilities[ability_to_toss].is_disabled = true
-    GameState.player_stats.max_shield += 25
-    GameState.player_stats.shield += 25
+func helpmove_event_callback():
+    var armorChance = randi_range(1,2)
+    if armorChance == 1:
+        GameState.player_stats.max_shield += 25
+        GameState.player_stats.shield += 25
+    else:
+        GameState.player_stats.max_shield = max(GameState.player_stats.max_shield - 25, 1)
+        GameState.player_stats.shield = max(GameState.player_stats.shield - 25, 1)
     SceneLoader.load_scene("res://scenes/map/map.tscn")
     
 #event4callback
@@ -192,11 +214,11 @@ func wildgoose_event_callback():
 func well_event_callback():
     var chance = randi_range(1,10)
     if chance <= 7:
-        GameState.player_stats.max_health = max(GameState.player_stats.max_health +20, 1)
-        GameState.player_stats.health = max(GameState.player_stats.max_health +20, 1)
-    else:
         GameState.player_stats.max_health = max(GameState.player_stats.max_health - 20, 1)
-        GameState.player_stats.health = max(GameState.player_stats.max_health - 20, 1)
+        GameState.player_stats.health = max(GameState.player_stats.max_health - 20, 1)  
+    else:
+        GameState.player_stats.max_health += 20
+        GameState.player_stats.health += 20            
     SceneLoader.load_scene("res://scenes/map/map.tscn") 
 
 func _on_reject_button_pressed() -> void:
